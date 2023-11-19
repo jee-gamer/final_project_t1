8e7a2845ef19cfe268a000443238d4154ab09a70
Jiramate Kedmake <jiramate2548@gmail.com>
2023-11-19 16:18:18 +0700

    Made Table and Database Class implemented login function all done

--- project_manage.py
+++ project_manage.py
@@ -1,17 +1,60 @@
 # import database module
+import random
 
+from database import Table, Database, read_csv
+import csv, os, copy
 # start by adding the admin related code
 
 # create an object to read an input csv file, persons.csv
 
 # create a 'persons' table
+persons_table = Table('persons', read_csv('persons.csv'))
 
 # add the 'persons' table into the database
+Database1 = Database()
+Database1.insert(persons_table)
 
 # create a 'login' table
+login_table = Table('login', [])
+
+for person in persons_table.table:
+    dictionary = {"person_id": person["ID"],
+                  "username": f'{person["first"]}.{person["last"][-1]}',
+                  "password": str(random.randrange(1000, 10000)),
+                  "role": person["type"]
+    }
+    login_table.insert(dictionary)
+
+Database1.insert(login_table)
+print(login_table)
+
+def login():
+    username = input('Username: ')
+    password = input('Password: ')
+
+    if len(password) != 4:
+        print("Password have to be 4 digit integers!")
+        return
+    try:
+        password = int(password)
+    except ValueError as e:
+        print(e, "// Password have to be 4 digit integers!")
+
+    if username not in [x["username"] for x in login_table.table]:
+        print("The username doesn't exist.")
+        return None
+    for _person in login_table.table:
+        if _person["username"] == username:
+            if _person["password"] == str(password):
+                print("Login successful")
+                return [_person["person_id"], _person["role"]]
+            else:
+                print("Wrong password!")
+                return None
+
+login()
 
 # the 'login' table has the following keys (attributes):
-
 # person_id
 # username
 # password
@@ -28,3 +71,4 @@
 # add the 'login' table into the database
 
 # add code that performs a login task; asking a user for a username and password; returning [person_id, role] if valid, otherwise returning None
+
